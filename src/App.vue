5b590c87229b6962baeb504591e8e1575cf7c770
IsabellaSoares <isabella.soares.28@gmail.com>
2023-01-29 23:37:28 -0300

    replace web banner

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,7 @@ import Footer from "./components/Footer.vue";
 <template>
   <v-app>
     <header>
-      <img class="logo" src="@/assets/web-banner.png" alt="Banner IWD 2023" />
-      <h1 class="saveTheDateTitle">25 de março, 2023</h1>
-      <h3 class="saveTheDateSubtitle">Presencial & Virtual</h3>
-      <strong class="saveTheDatePlace">São Paulo | 13:00 - 18:00</strong>
+      <img class="logo" src="@/assets/web-banner.svg" alt="Banner IWD 2023" />
     </header>
 
     <RouterView />
@@ -19,43 +16,10 @@ import Footer from "./components/Footer.vue";
 </template>
 
 <style scoped>
-@media (max-width: 1024px) {
-  .saveTheDateTitle {
-    visibility: hidden;
-  }
-  .saveTheDateSubtitle {
-    visibility: hidden;
-  }
-  .saveTheDatePlace {
-    visibility: hidden;
-  }
-}
-
 @media (min-width: 1024px) {
   .img {
     width: 30%;
   }
-}
-
-.saveTheDateTitle {
-  position: absolute;
-  bottom: 150px;
-  left: 40px;
-  font-weight: 600;
-}
-
-.saveTheDateSubtitle {
-  position: absolute;
-  bottom: 110px;
-  left: 40px;
-  font-weight: 400;
-}
-
-.saveTheDatePlace {
-  position: absolute;
-  bottom: 70px;
-  left: 40px;
-  font-weight: 600;
 }
 
 header {
