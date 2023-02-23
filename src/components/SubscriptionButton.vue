daac935a4026f962e882cd45541964b897ec62b8
IsabellaSoares <isabella.soares.28@gmail.com>
2023-02-23 00:07:59 -0300

    alterar infos sobre presencial

--- src/components/SubscriptionButton.vue
+++ src/components/SubscriptionButton.vue
@@ -17,16 +17,35 @@ export default {
   <section>
     <Title text="Inscrições Abertas!" />
     <p class="content">
-      Estão <strong>ABERTAS AS INSCRIÇÕES</strong> para o IWD Sudeste 2023. Esse evento será
-      realizado na <strong>Google São Paulo</strong>, no dia <strong>25/03/2023</strong>, das 13h00min as
-      18h00min, em duas modalidades <strong>GRATUITAS</strong>: online e presencial. O presencial
-      é <strong>LIMITADO</strong> e <strong>EXCLUSIVO</strong> para mulheres (cis e trans). Por gentileza, só
-      adquira o ingresso presencial caso tenha <strong>CERTEZA</strong> de comparecimento no
-      evento. Entraremos em contato pelo e-mail <strong>iwdsudeste@gmail.com</strong> As
-      inscrições podem ser realizadas através do botão abaixo
+      Estão <strong>ABERTAS AS INSCRIÇÕES</strong> para o IWD Sudeste 2023. Esse
+      evento será realizado na <strong>Google São Paulo</strong>, no dia
+      <strong>25/03/2023</strong>, das 13h00min as 18h00min, em duas modalidades
+      <strong>GRATUITAS</strong>: online e presencial. O presencial é
+      <strong>LIMITADO</strong> e <strong>EXCLUSIVO</strong> para mulheres (cis
+      e trans). No momento, as incrições para o evento
+      <strong>PRESENCIAL ESTÃO ESGOTADAS</strong>. Para as inscritas, entraremos
+      em contato pelo e-mail <strong>iwdsudeste@gmail.com</strong>.
+    </p>
+    <p class="content">
+      Para o evento presencial serão <strong>OBRIGATÓRIOS</strong> os
+      <span style="color: red"
+        >comprovantes de vacinação da primeira e da segunda dose da vacina para
+        COVID-19</span
+      >
+      e <span style="color: red">documento de identidade com foto</span>.
+    </p>
+    <p class="content">
+      As inscrições podem ser realizadas através do botão abaixo
     </p>
     <v-row justify="center" class="my-4">
-      <div class="btn" @click="goTo('https://www.sympla.com.br/evento/iwd-sudeste-2023/1873998?lang=PT')">
+      <div
+        class="btn"
+        @click="
+          goTo(
+            'https://www.sympla.com.br/evento/iwd-sudeste-2023/1873998?lang=PT'
+          )
+        "
+      >
         <h3>Inscreva-se aqui!</h3>
       </div>
     </v-row>
