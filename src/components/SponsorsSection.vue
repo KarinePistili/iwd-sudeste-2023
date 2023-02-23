1f8f97d1ad2e1d9d4c90723145be1c1a62040cb2
IsabellaSoares <isabella.soares.28@gmail.com>
2023-02-23 00:07:33 -0300

    adicionar patrocinio OneBitCode e GDG

--- src/components/SponsorsSection.vue
+++ src/components/SponsorsSection.vue
@@ -1,6 +1,8 @@
 <script setup>
 import Title from "./SectionTitle.vue";
 import gCloudLogo from "../assets/google-cloud-logo.png";
+import gDevelopersGroupLogo from "../assets/google-developers-logo.jpg";
+import oneBitCodeLogo from "../assets/onebitcode-logo.png";
 </script>
 
 <script>
@@ -11,6 +13,14 @@ export default {
         src: gCloudLogo,
         flex: 3,
       },
+      {
+        src: gDevelopersGroupLogo,
+        flex: 3,
+      },
+      {
+        src: oneBitCodeLogo,
+        flex: 3,
+      },
     ],
   }),
 };
@@ -19,10 +29,29 @@ export default {
 <template>
   <section>
     <Title text="Patrocinadores" />
-    <v-row style="width: 100vw" class="pa-12" justify="center">
+    <span class="cota">
+      <strong>Cota Dare To Be</strong>
+    </span>
+    <v-row style="width: 100vw" class="pa-12 pt-3" justify="center">
       <v-col cols="12" lg="3" md="4" sm="6" xs="12">
         <v-card width="100%" class="px-5">
           <v-img :src="gCloudLogo" class="align-end" height="200px"> </v-img>
+        </v-card>
+      </v-col>
+      <v-col cols="12" lg="3" md="4" sm="6" xs="12">
+        <v-card width="100%" class="px-5">
+          <v-img :src="gDevelopersGroupLogo" class="align-end" height="200px">
+          </v-img>
+        </v-card>
+      </v-col>
+    </v-row>
+    <span class="cota">
+      <strong>Cota Innovative</strong>
+    </span>
+    <v-row style="width: 100vw" class="pa-12 pt-3" justify="center">
+      <v-col cols="12" lg="3" md="4" sm="6" xs="12">
+        <v-card width="100%" class="px-5">
+          <v-img :src="oneBitCodeLogo" class="align-end" height="200px"> </v-img>
         </v-card>
       </v-col>
     </v-row>
@@ -32,5 +61,10 @@ export default {
 <style scoped>
 section {
   margin-top: 60px;
+}
+
+.cota {
+  font-size: 1.5rem;
+  color: var(--vt-c-blue);
 }
 </style>
