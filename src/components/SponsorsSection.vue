48ce62f3d8c702a616538042aafbd489b9e77061
IsabellaSoares <isabella.soares.28@gmail.com>
2023-03-01 22:34:03 -0300

    adicionar mais patrocinios

--- src/components/SponsorsSection.vue
+++ src/components/SponsorsSection.vue
@@ -4,6 +4,11 @@ import gCloudLogo from "../assets/google-cloud-logo.png";
 import gDevelopersGroupLogo from "../assets/google-developers-logo.jpg";
 import oneBitCodeLogo from "../assets/onebitcode-logo.png";
 import nearSureLogo from "../assets/nearsure-logo.png";
+import cloudBedsLogo from "../assets/cloudbeds-logo.png";
+import gabulDevLogo from "../assets/gabuldev-logo.png";
+import totvsLogo from "../assets/totvs-logo.png";
+import wtmLogo from "../assets/wtm-patrocinio-logo.png";
+import unimarLogo from "../assets/UnimarSJ-logo.svg";
 </script>
 
 <script>
@@ -23,7 +28,27 @@ export default {
         flex: 3,
       },
       {
-        src: oneBitCodeLogo,
+        src: nearSureLogo,
+        flex: 3,
+      },
+      {
+        src: cloudBedsLogo,
+        flex: 3,
+      },
+      {
+        src: wtmLogo,
+        flex: 3,
+      },
+      {
+        src: gabulDevLogo,
+        flex: 3,
+      },
+      {
+        src: totvsLogo,
+        flex: 3,
+      },
+      {
+        src: unimarLogo,
         flex: 3,
       },
     ],
@@ -49,6 +74,11 @@ export default {
           </v-img>
         </v-card>
       </v-col>
+      <v-col cols="12" lg="3" md="4" sm="6" xs="12">
+        <v-card width="100%" class="px-5">
+          <v-img :src="wtmLogo" class="align-end" height="200px"> </v-img>
+        </v-card>
+      </v-col>
     </v-row>
     <span class="cota">
       <strong>Cota Innovative</strong>
@@ -56,7 +86,18 @@ export default {
     <v-row style="width: 100vw" class="pa-12 pt-3" justify="center">
       <v-col cols="12" lg="3" md="4" sm="6" xs="12">
         <v-card width="100%" class="px-5">
-          <v-img :src="oneBitCodeLogo" class="align-end" height="200px"> </v-img>
+          <v-img :src="oneBitCodeLogo" class="align-end" height="200px">
+          </v-img>
+        </v-card>
+      </v-col>
+      <v-col cols="12" lg="3" md="4" sm="6" xs="12">
+        <v-card width="100%" class="px-5">
+          <v-img :src="gabulDevLogo" class="align-end" height="200px"> </v-img>
+        </v-card>
+      </v-col>
+      <v-col cols="12" lg="3" md="4" sm="6" xs="12">
+        <v-card width="100%" class="px-5">
+          <v-img :src="unimarLogo" class="align-end" height="200px"> </v-img>
         </v-card>
       </v-col>
     </v-row>
@@ -67,6 +108,16 @@ export default {
       <v-col cols="12" lg="3" md="4" sm="6" xs="12">
         <v-card width="100%" class="px-5">
           <v-img :src="nearSureLogo" class="align-end" height="200px"> </v-img>
+        </v-card>
+      </v-col>
+      <v-col cols="12" lg="3" md="4" sm="6" xs="12">
+        <v-card width="100%" class="px-5">
+          <v-img :src="cloudBedsLogo" class="align-end" height="200px"> </v-img>
+        </v-card>
+      </v-col>
+      <v-col cols="12" lg="3" md="4" sm="6" xs="12">
+        <v-card width="100%" class="px-5">
+          <v-img :src="totvsLogo" class="align-end" height="200px"> </v-img>
         </v-card>
       </v-col>
     </v-row>
